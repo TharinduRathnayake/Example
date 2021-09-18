4ec3b15c5d9f6372a6465ae762d0575933643f69
Umayangi Ekanayake <89120566+umayangi12@users.noreply.github.com>
2021-09-18 12:02:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -124,6 +124,7 @@ app.use(postRoutes);
 
 //......................Test 01.........................................................
 
+//test000222
 
 const PORT = 8000;
 const DB_URL = 'mongodb+srv://itp123:itp123@visioncargo.p4cvw.mongodb.net/VisionCargo?retryWrites=true&w=majority'
